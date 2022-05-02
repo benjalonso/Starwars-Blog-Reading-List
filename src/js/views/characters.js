cf42dd9bba169d53612ced03c448e54fe3c2dbe8
Attackama-Bwoy <manuel.jh.escalera@gmail.com>
2022-05-02 15:28:16 +0000

    star V2

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -5,29 +5,36 @@ import { Pagination } from "../component/Pagination";
 export const Characters = () => {
 
 	const [characters, setCharacters] = useState([]);
-	const [info, setInfo] = useState({});
 
 	const initialUrl = "https://www.swapi.tech/api/people"
-
+	
 	const fetchCharacters = (initialUrl) =>{
 		fetch (initialUrl)
 		.then (response => response.json())
-		.then (data => {
-			setCharacters(data.results);
-			setInfo(data.info);
-		})
+		.then (data => setCharacters(data.results))
 		.catch (error => console.log(error))
 	}
 
+		/* .then (data => {
+			const {results} = data;
+			results.forEach(async(characters, index) => {
+				const resp = await fetch (characters.url);
+				const info = await resp.jason();
+				data.results[index] = info;
+				console.log(info)
+			});
+		}) */
+	
 	useEffect(() => {
 	  fetchCharacters(initialUrl);
 	}, [])
 	
 	return (
 	<div>
-		<Pagination prev={info.previus} next={info.next}/>
+		{/* <Pagination prev={info.previus} next={info.next}/> */}
 		<FormatoInfo characters={characters} img={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png"} />
-		<Pagination />
+		{/* <Pagination /> */}
 	</div>
 	);
+	
 };
